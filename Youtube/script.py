2f8acf07884e5af22772639a87fe4aa570e9897d
MAYANK JAAT <mayankadsens1@gmail.com>
2025-07-17 10:01:46 +0530

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -4,12 +4,12 @@ class Translation(object):
 
     ABOUT_TXT = """
 ╭───────────⍟
-├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
+├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeVideoDownloader0Bot)
 ├📢 **Framework** : [PyroFork 2.3.50](https://docs.pyrogram.org/)
 ├💮 **Language** : [Python 3.12.6](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
+├👥 **Support Group** : [Warrior Bots Support](https://t.me/+XAtFZIm4GNZmNGNl)
+├🥏 **Channel** : [Warrior Bots Updates](https://t.me/WarriorUnitsBots)
+├⛲ **Source** : [Buy Boy Source Code](https://t.me/oo7jatji)
+├🎓 **Developer** : [Stranger](https://t.me/oo7jatji)
 ╰───────────────⍟
 """
