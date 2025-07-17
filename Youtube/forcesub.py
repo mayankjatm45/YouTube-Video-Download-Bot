37a6172a58d45b91e8e1030824581338f095789f
MAYANK JAAT <mayankadsens1@gmail.com>
2025-07-17 10:04:28 +0530

    Update forcesub.py

--- Youtube/forcesub.py
+++ Youtube/forcesub.py
@@ -18,7 +18,7 @@ async def handle_force_subscribe(bot, message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=message.from_user.id,
-                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
+                text="Sorry Sir, You are Banned. Contact My [Support Group](https://t.me/+XAtFZIm4GNZmNGNl).",
                 disable_web_page_preview=True,
             )
             return 400
@@ -39,7 +39,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/NT_BOTS_SUPPORT).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/+XAtFZIm4GNZmNGNl).",
             disable_web_page_preview=True,
         )
         return 400
